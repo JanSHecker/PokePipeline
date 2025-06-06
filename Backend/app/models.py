f379808a3e67c4be08eb6291428063cb17d07426
JanSHecker <jan.hecker00@gmail.com>
2025-06-06 14:59:05 +0200

    fix delete mutation

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -6,13 +6,31 @@ class Pokemon(Base):
     __tablename__ = "pokemon"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    types = relationship("Type", secondary="pokemon_type_association")
-    moves = relationship("Move", secondary="pokemon_move_association")
+    types = relationship(
+        "Type", 
+        secondary="pokemon_type_association", 
+        back_populates="pokemon",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+    moves = relationship(
+        "Move", 
+        secondary="pokemon_move_association", 
+        back_populates="pokemon",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 class Type(Base):
     __tablename__ = "type"
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
+    
+    pokemon = relationship(
+        "Pokemon",
+        secondary="pokemon_type_association",
+        back_populates="types",
+    )
 
 class Move(Base):
     __tablename__ = "move"
@@ -24,15 +42,22 @@ class Move(Base):
     type_id = Column(Integer, ForeignKey("type.id"))
     type = relationship("Type")
 
-# Association Tables
+ 
+    pokemon = relationship(
+        "Pokemon",
+        secondary="pokemon_move_association",
+        back_populates="moves",
+    )
+
+# Association Tables 
 pokemon_type_association = Table(
     "pokemon_type_association", Base.metadata,
-    Column("pokemon_id", Integer, ForeignKey("pokemon.id")),
-    Column("type_id", Integer, ForeignKey("type.id"))
+    Column("pokemon_id", Integer, ForeignKey("pokemon.id", ondelete="CASCADE")),
+    Column("type_id", Integer, ForeignKey("type.id", ondelete="CASCADE")),
 )
 
 pokemon_move_association = Table(
     "pokemon_move_association", Base.metadata,
-    Column("pokemon_id", Integer, ForeignKey("pokemon.id")),
-    Column("move_id", Integer, ForeignKey("move.id"))
+    Column("pokemon_id", Integer, ForeignKey("pokemon.id", ondelete="CASCADE")),
+    Column("move_id", Integer, ForeignKey("move.id", ondelete="CASCADE")),
 )
